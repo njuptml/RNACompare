50a7d0b3a9affb5976a3d3a49c727aa13a1d5c2a
Yusheng gao <Yusheng gao>
2016-12-17 16:45:20 +0800

    remove

--- IndefiniteSVM/subRoutines/weightPenalty.m
+++
@@ -1,9 +0,0 @@
-% return a vector of penalties where Cweighted(i)=C if labels(i)=1
-% and Cweighted(i)=weight*C if labels(i)=-1
-%
-% Ronny Luss and Alexandre d'Aspremont, last modification: March 2008
-
-function Cweighted = weightPenalty(C,labels,weight)
-
-Cweighted=C*ones(length(labels),1);
-Cweighted(labels<0)=Cweighted(labels<0)*weight;
